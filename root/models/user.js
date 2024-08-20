c7e09a59c1e2ad2086472f8dfc5b1acb6c67233f
chris <cvera8873@gmail.com>
2024-08-19 19:46:15 -0500

    updated skeleton config and models

---
+++ root/models/user.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (user) => {
+        user.password = await bcrypt.hash(user.password, 10);
+        return user;
+      },
+    },
+    sequelize,
+    modelName: 'User',
+  }
+);
+
+module.exports = User;
